cd7fc7a40c3f410a169d7a83ccaf4362eaac228e
Nikita Singh <niki.singh1612@gmail.com>
2024-04-09 22:05:17 +0530

    implemented signup page

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import SignUp from "./components/SignUp";
+import CreateProfile from "./components/CreateProfile";
+function App() {
+  return (
+    <div>
+      <SignUp></SignUp>
+      <CreateProfile></CreateProfile>
+    </div>
+  );
+}
+export default App;
