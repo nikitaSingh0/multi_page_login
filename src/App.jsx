e02c19af7daa61ca86a4be79f15ead2a3bb3a621
Nikita Singh <niki.singh1612@gmail.com>
2024-04-10 23:07:19 +0530

    implemented multi-page-login flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import SignUp from "./components/SignUp";
-import CreateProfile from "./components/CreateProfile";
+import SignUp from "./pages/sign_up/SignUp";
+import CreateProfile from "./pages/profile/CreateProfile";
+import UserInfo from "./pages/user_info/UserInfo";
+import EmailVerification from "./pages/verify_email/EmailVerification";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <div>
-      <SignUp></SignUp>
-      <CreateProfile></CreateProfile>
+      <Router>
+        <Routes>
+          <Route path="/" element={<SignUp />} />
+          <Route path="/profile" element={<CreateProfile />} />
+          <Route path="/user-info" element={<UserInfo />} />
+          <Route path="/verify-email" element={<EmailVerification />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
